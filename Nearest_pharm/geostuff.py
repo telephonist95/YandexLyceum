edde21b320c3a9265666184931dfeeeb8d208e8f
telephonist95 <telephonist95@gmail.com>
2022-02-05 20:39:34 +0300

    snippet

--- Nearest_pharm/geostuff.py
+++ Nearest_pharm/geostuff.py
@@ -1,6 +1,22 @@
 import requests
 from PIL import Image
 from io import BytesIO
+import math
+
+
+def lonlat_distance(a, b):
+    degree_to_meters_factor = 111 * 1000 # 111 километров в метрах
+    a_lon, a_lat = a
+    b_lon, b_lat = b
+    # Берем среднюю по широте точку и считаем коэффициент для нее.
+    radians_lattitude = math.radians((a_lat + b_lat) / 2.)
+    lat_lon_factor = math.cos(radians_lattitude)
+    # Вычисляем смещения в метрах по вертикали и горизонтали.
+    dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
+    dy = abs(a_lat - b_lat) * degree_to_meters_factor
+    # Вычисляем расстояние между точками.
+    distance = math.sqrt(dx * dx + dy * dy)
+    return distance
 
 
 def get_coords_spn(toponym):
@@ -39,8 +55,11 @@ def get_org(ll, org=''):
         json_response = response.json()
         organization = json_response["features"][0]
         point = organization["geometry"]["coordinates"]
-        return point
-    return None, None
+        name = organization['properties']["name"]
+        ad = organization['properties']['CompanyMetaData']['address']
+        time = organization['properties']['CompanyMetaData']['Hours']['text']
+        return *point, name, ad, time
+    return None, None, None, None
 
 
 def show_map(ll='', type_map='map', spn='', point=""):
--- Nearest_pharm/main.py
+++ Nearest_pharm/main.py
@@ -1,4 +1,4 @@
-from geostuff import get_coords_spn, show_map, get_org
+from geostuff import get_coords_spn, show_map, get_org, lonlat_distance
 import sys
 
 
@@ -7,9 +7,13 @@ def main():
     try:
         lat, lon, spn = get_coords_spn(toponym)
         ll = f"{lat},{lon}"
-        lat_org, lon_org = get_org(ll, org='аптека')
+        lat_org, lon_org, name, adr, time = get_org(ll, org='аптека')
         ll_org = f"{lat_org},{lon_org}"
         show_map(point='~'.join((ll, ll_org)))
+        print("Distance:", lonlat_distance(map(float, ll.split(',')), map(float, ll_org.split(','))))
+        print("Address:", adr)
+        print("Name:", name)
+        print("Time:", time)
     except Exception as e:
         print('error', e)
 
